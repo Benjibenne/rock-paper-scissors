d186e4c08914671cbefece9cb6c3cb6fb77fcade
Benjamin Solberg <benjiwow99@gmail.com>
2022-09-20 00:05:39 +0000

    add transform scale to css to style button hover when mouse hover over them and added keywords with values in js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/* buttons for rock paper scissors 
+for player to click on */
+let rock-btn;
+let paper-btn;
+let scissor-btn;
+
+/* player and computer images */
+let players-choices
+let computers-choices
+
+/* score points */
+let player-score = 0;
+let computer-score = 0;
+
